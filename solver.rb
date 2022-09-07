ba5e82458a9a1375079b12e7144a538a28e7f139
Shubh <shubh.scb@gmail.com>
2022-09-07 21:22:38 +0530

    REVERSE A STRING TEST PASS

--- solver.rb
+++ solver.rb
@@ -6,4 +6,12 @@ class Solver
             return num * factorial(num-1)
         end
     end
+
+    def reverse(word)
+        if word.length == 1
+            return word
+        else
+            return word[-1] + reverse(word[0..-2])
+        end
+    end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -22,3 +22,26 @@ describe Solver do
         expect(solver.factorial(4)).to eq(24)
     end
 end
+
+describe Solver do
+    it "should return 'a' when given 'a'" do
+        solver = Solver.new
+        expect(solver.reverse("a")).to eq("a")
+    end
+    it "should return 'ba' when given 'ab'" do
+        solver = Solver.new
+        expect(solver.reverse("shubh")).to eq("hbuhs")
+    end
+    it "should return 'cba' when given 'abc'" do
+        solver = Solver.new
+        expect(solver.reverse("microverse")).to eq("esrevorcim")
+    end
+    it "should return 'dcba' when given 'abcd'" do
+        solver = Solver.new
+        expect(solver.reverse("michael")).to eq("leahcim")
+    end
+    it "should return 'kurb' when given 'bruk'" do
+        solver = Solver.new
+        expect(solver.reverse("bruk")).to eq("kurb")
+    end
+end
